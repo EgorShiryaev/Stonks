d9774b370da884da31a9430bdfe39f68c2235040
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-03-14 00:20:40 +0300

    Добавлен appbar и стили для приложения

---
+++ lib/widgets/current_date.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CurrentDate extends StatefulWidget {
+  const CurrentDate({Key? key}) : super(key: key);
+
+  @override
+  State<CurrentDate> createState() => _CurrentDateState();
+}
+
+class _CurrentDateState extends State<CurrentDate> {
+  DateTime dateNow = DateTime.now();
+  bool changeTimer = true;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    Intl.defaultLocale = 'ru';
+  }
+
+  setTimer() {
+    if (changeTimer) {
+      final durationToNextDayInSeconds =
+          DateTime(dateNow.year, dateNow.month, dateNow.day)
+              .add(const Duration(days: 1))
+              .difference(dateNow);
+      timer = Timer(durationToNextDayInSeconds, () {
+        setState(() {
+          dateNow = DateTime.now();
+          changeTimer = true;
+        });
+        timer!.cancel();
+      });
+    } else {
+      setState(() {
+        changeTimer = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    setTimer();
+    return Text(
+      DateFormat.yMMMMd().format(dateNow),
+      style: Theme.of(context).textTheme.subtitle2,
+    );
+  }
+
+  @override
+  void dispose() {
+    timer!.cancel();
+    super.dispose();
+  }
+}
