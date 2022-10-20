75ab5e10c245ddb0430ba95b9611384a56620d49
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-10-20 15:08:58 +0300

    update token

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,12 +1,12 @@
 class SETTINGS {
-  static const _token = 'c8n2dviad3id1m4i8emg';
-  static const _sandboxToken = 'sandbox_c8n2dviad3id1m4i8en0';
   static const _websocketBaseUrl = 'wss://ws.finnhub.io';
   static const _baseUrl = 'https://finnhub.io/api/v1';
   static const stocksLocalDataSourcesId = 'box_for_stocks';
 
   static String get websocketUrl => '$_websocketBaseUrl?token=$_token';
 
-  static Uri getUrl(String path) =>
-      Uri.parse('$_baseUrl/$path&token=$_sandboxToken');
+ static const _token = 'cd8jhfqad3i5t4lmarl0cd8jhfqad3i5t4lmarlg';
+  static Uri getUrl(String path) {
+    return Uri.parse('$_baseUrl/$path&token=$_token');
+  }
 }
