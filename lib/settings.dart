f60c5bae5e7d96e46ecb456d4e8bc28b108be8b2
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-10-20 15:04:29 +0300

    update api key

--- lib/settings.dart
+++ lib/settings.dart
@@ -2,8 +2,8 @@ import 'package:stonks/datasources/app_info_local_datasource.dart';
 import 'package:stonks/datasources/stocks_local_datasources.dart';
 
 class SETTINGS {
-  static const _token = 'c8n2dviad3id1m4i8emg';
-  static const _sandboxToken = 'sandbox_c8n2dviad3id1m4i8en0';
+  static const _token = 'cd8jhfqad3i5t4lmarl0cd8jhfqad3i5t4lmarlg';
+
   static const _websocketBaseUrl = 'wss://ws.finnhub.io';
   static const _baseUrl = 'https://finnhub.io/api/v1';
   static const appInfoLocalDataSourceUrl = 'app_info';
@@ -11,6 +11,7 @@ class SETTINGS {
 
   static get websocketUrl => Uri.parse('$_websocketBaseUrl?token=$_token');
 
-  static getUrl(String path) =>
-      Uri.parse('$_baseUrl/$path&token=$_sandboxToken');
+  static Uri getUrl(String path) {
+    return Uri.parse('$_baseUrl/$path&token=$_token');
+  }
 }
